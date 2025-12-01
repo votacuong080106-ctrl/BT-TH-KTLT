bce1b002d749b2a86c3baf2c28ce68d261d4e1e2
votacuong080106-ctrl <votacuong080106@gmail.com>
2025-12-02 00:12:28 +0700

    Delete bài1 directory

--- bài1/bai3.py
+++
@@ -1,7 +0,0 @@
-# Võ Tá Cường , MSV: 245752021610100
-n = 3int(input("Enter a number --> "))
-
-if n % 2 == 0:
-    print("EVEN Number")
-else:
-    print("ODD Number")  
--- bài1/bai5.py
+++
@@ -1,8 +0,0 @@
-# Võ Tá Cường , MSV: 245752021610100
-# Nhập số tự nhiên n > 0
-n = int(input("Nhập số tự nhiên n (>0): "))
-
-# In các số tự nhiên giảm dần từ n đến 0
-for i in range(n, 0 - 1, -1):
-    print(i)
-
