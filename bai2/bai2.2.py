071566802f58604772f06673e1d6e5b26501b7da
votacuong080106-ctrl <votacuong080106@gmail.com>
2025-12-04 09:10:32 +0700

    Delete bai2 directory

--- bai2/bai2.2.py
+++
@@ -1,6 +0,0 @@
-# Võ Tá Cường , MSV: 245752021610100
-def sum(a, b):
-    return a + b
-
-c = sum(4, 5)
-print("Tổng của 4 và 5 =", c)
--- bai2/bai2.7.py
+++
@@ -1,8 +0,0 @@
-# Võ Tá Cường , MSV: 245752021610100
-def checkValue(n):
-    if n % 2 == 0:
-        print("Đây là một số chẵn")
-    else:
-        print("Đây là một số lẻ")
-
-checkValue(7)
